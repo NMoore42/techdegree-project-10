d824b67d40e2c93a263230130c723e1f843c259b
NMoore42 <nicholas.moore5@icloud.com>
2019-07-22 10:16:49 -0500

    rough routes and index view created

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # techdegree-project-10
 Project 10 for the Team Treehouse Fullstack JavaScript TechDegree Program
+
+This is a CRUD app to help manage a fictional library's collection of books.
+
+
+The finished project includes pages to list, add, update, and delete books.
+
+
+This app was built using JavaScript, Node.js, Express, Pug, and Sequelize.
---
+++ app.js
@@ -0,0 +1,62 @@
+//app.js modified from sequelize_blog_video_8_finished
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+
+
+const routes = require('./routes/index');
+const books = require('./routes/books');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/books', books);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
