f25c0f2b30f1f3150d38b14ca04a4ee4b3eff913
NMoore42 <nicholas.moore5@icloud.com>
2019-07-23 17:23:39 -0500

    completed error handling for page-not-found

--- app.js
+++ app.js
@@ -1,62 +1,38 @@
 //app.js modified from sequelize_blog_video_8_finished
-
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
-
-
 const routes = require('./routes/index');
 const books = require('./routes/books');
-
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handlers
-
-
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  err.status === 404 ? res.render("page-not-found") : res.render("error")
 });
-
 
 module.exports = app;
